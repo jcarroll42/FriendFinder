36291b50c53106c6f32e040e82d2f62567ac91b3
Josh Carroll <Josh Carroll>
2016-07-23 13:16:11 -0500

    adding stuff for heroku

--- app/server.js
+++ app/server.js
@@ -3,8 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
-var port = 3333;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -13,8 +11,6 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(express.static('public'));
 
 require('./routing/html-routes.js')(app);
-require('./routing/api-routes.js')(app);
+require('./app/routing/api-routes.js')(app);
 
-app.listen(port, function(){
-	console.log("App listening on port " + port);
-});
+app.listen(process.env.PORT || 3000);
